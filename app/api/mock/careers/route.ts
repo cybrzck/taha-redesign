13380c5c604424b3dce6399e29ff5c21c68fe2d6
Izack Kimario <153287188+cybrzck@users.noreply.github.com>
2026-09-22 11:43:18 +0300

    Update route.ts

--- app/api/mock/careers/route.ts
+++ app/api/mock/careers/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { mockData } from "@/data/mockData";
 //hello
+//
 export async function GET() {
     return NextResponse.json({
         success: true,
